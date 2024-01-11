97103435b8e4669c17b04e9e2d7a997b19959e43
Fillonit Ibishi <filloniti@gmail.com>
2024-01-11 22:59:48 +0100

    Remove unused imports in PostPage and main.tsx

--- client/src/components/PostPage/PostPage.tsx
+++ client/src/components/PostPage/PostPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import PostCard from "./PostCard";
 import PostContent from "./PostContent";
 // import PostComments from "./PostComents";
