b965d4885ac78df50700d3d822a33b663422ccf7
butrinnt_ <butritn.reqica@gmail.com>
2024-01-13 19:33:41 +0100

    wokring on settings

--- client/src/components/Profile/ProfileSettings.tsx
+++ client/src/components/Profile/ProfileSettings.tsx
@@ -1,182 +1,283 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, TextInput, FileInput, Label } from "flowbite-react";
-// import { API_BASE_URL } from "../../config";
+import { API_BASE_URL } from "../../config";
+import { ToastContainer, UpdateOptions, toast } from "react-toastify";
+import notifyConfig from "../notifyConfig";
 
 const inputTheme = {
-	field: {
-		input: {
-			colors: {
-				purple: "border-purple-500 bg-purple-50 text-purple-900 placeholder-purple-700 focus:border-purple-500 focus:ring-purple-500 dark:border-purple-400 dark:bg-purple-100 dark:focus:border-purple-500 dark:focus:ring-purple-500 ",
-			},
-		},
-	},
+  field: {
+    input: {
+      colors: {
+        purple:
+          "border-purple-500 bg-purple-50 text-purple-900 placeholder-purple-700 focus:border-purple-500 focus:ring-purple-500 dark:border-purple-400 dark:bg-purple-100 dark:focus:border-purple-500 dark:focus:ring-purple-500 ",
+      },
+    },
+  },
 };
 
+// interface UserData {
+//     username: string;
+//     email: string;
+//     profile: {
+//         firstName: string;
+//         lastName: string;
+//         avatar: string;
+//         bio: string;
+//     };
+//     authentication: {
+//         password: string;
+//         salt: string;
+//         sessionToken: string;
+//     };
+//     _id: string;
+//     role: string;
+//     createdAt: string;
+//     updatedAt: string;
+//     __v: number;
+// }
+
 interface User {
-	username: string;
-	email: string;
-	firstName: string;
-	lastName: string;
-	password: string;
-	profilePicture: string;
-	bio: string;
+  _id: string;
+  username: string;
+  email: string;
+  profile: {
+    firstName: string;
+    lastName: string;
+    avatar: string;
+    bio: string;
+  };
+  authentication: {
+    password: string;
+    sessionToken: string;
+  };
 }
 
 const SettingsPage: React.FC = () => {
-	const [user, setUser] = useState<User>({
-		username: "",
-		email: "",
-		firstName: "",
-		lastName: "",
-		password: "",
-		profilePicture: "",
-		bio: "",
-	});
+  const [user, setUser] = useState<User>({
+    _id: "",
+    username: "",
+    email: "",
+    profile: { firstName: "", lastName: "", avatar: "", bio: "" },
+    authentication: { password: "", sessionToken: "" },
+  });
 
-	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setUser({
-			...user,
-			[event.target.name]: event.target.value,
-		});
-	};
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUser({
+      ...user,
+      [event.target.name]: event.target.value,
+    });
+  };
 
-	const handleSubmit = (event: React.FormEvent) => {
-		event.preventDefault();
-		// Handle form submission here
-	};
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
 
-	const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		if (event.target.files && event.target.files[0]) {
-			const reader = new FileReader();
+    const SignUpToast = toast.loading("Updating Profile...", {
+      autoClose: 3000,
+    });
+    try {
+      const response = await fetch(`${API_BASE_URL}/users/${user._id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authentication: user.authentication.sessionToken,
+        },
+        body: JSON.stringify({
+          user,
+        }),
+      });
+      const data = await response.json();
+      if (response.status === 201) {
+        toast.update(SignUpToast, {
+          render: "Profile Updated Successfully!",
+          type: "success",
+          ...notifyConfig,
+        } as UpdateOptions<unknown>);
+        // setLogIn(true);
+      } else {
+        toast.update(SignUpToast, {
+          render: "Profile Update Failed!",
+          type: "error",
+          ...notifyConfig,
+        } as UpdateOptions<unknown>);
+      }
+      console.log(data);
+    } catch (error) {
+      toast.update(SignUpToast, {
+        render: "Profile Update Failed!",
+        type: "error",
+        ...notifyConfig,
+      } as UpdateOptions<unknown>);
+      console.error(error);
+    }
+  };
 
-			reader.onload = (e) => {
-				setUser({
-					...user,
-					profilePicture: e.target?.result as string,
-				});
-			};
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
 
-			reader.readAsDataURL(event.target.files[0]);
-		}
-	};
+      reader.onload = (e) => {
+        setUser((prevUser) => ({
+          ...prevUser,
+          profile: {
+            ...prevUser.profile,
+            avatar: e.target?.result as string,
+          },
+        }));
+      };
 
-	return (
-		<div className="flex justify-center mt-6">
-			<div className="w-full max-w-md">
-				<form
-					onSubmit={handleSubmit}
-					className="bg-white shadow rounded px-8 pt-6 pb-8 mb-4"
-				>
-					<div className="mb-4">
-						<label
-							className="block text-gray-700 text-sm font-bold mb-2"
-							htmlFor="username"
-						>
-							Username
-						</label>
-						<TextInput
-							name="username"
-							placeholder="Username"
-							type="text"
-							value={user.username}
-							onChange={handleChange}
-							theme={inputTheme}
-							color={"purple"}
-							// className="border-purple-500 bg-purple-50 text-purple-900 placeholder-purple-700 focus:border-purple-500 focus:ring-purple-500 dark:border-purple-400 dark:bg-purple-100 dark:focus:border-purple-500 dark:focus:ring-purple-500"
-						/>
-					</div>
-					<div className="mb-4">
-						<label
-							className="block text-gray-700 text-sm font-bold mb-2"
-							htmlFor="email"
-						>
-							Email
-						</label>
-						<TextInput
-							name="email"
-							placeholder="Email"
-							type="email"
-							value={user.email}
-							onChange={handleChange}
-							theme={inputTheme}
-							color={"purple"}
-							className="appearance-none rounded w-full py-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-						/>
-					</div>
-					<div className="mb-4">
-						<label
-							className="block text-gray-700 text-sm font-bold mb-2"
-							htmlFor="firstName"
-						>
-							First Name
-						</label>
-						<TextInput
-							name="firstName"
-							placeholder="First Name"
-							type="text"
-							value={user.firstName}
-							onChange={handleChange}
-							theme={inputTheme}
-							color={"purple"}
-							className="appearance-none rounded w-full py-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-						/>
-					</div>
-					<div className="mb-4">
-						<label
-							className="block text-gray-700 text-sm font-bold mb-2"
-							htmlFor="lastName"
-						>
-							Last Name
-						</label>
-						<TextInput
-							name="lastName"
-							placeholder="Last Name"
-							type="text"
-							value={user.lastName}
-							onChange={handleChange}
-							theme={inputTheme}
-							color={"purple"}
-							className="appearance-none rounded w-full py-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-						/>
-					</div>
-					<div className="mb-4">
-						<label
-							className="block text-gray-700 text-sm font-bold mb-2"
-							htmlFor="password"
-						>
-							Password
-						</label>
-						<TextInput
-							name="password"
-							placeholder="Password"
-							type="password"
-							value={user.password}
-							onChange={handleChange}
-							theme={inputTheme}
-							color={"purple"}
-							className="appearance-none rounded w-full py-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-						/>
-					</div>
-					<div className="mb-4">
-						<div id="fileUpload" className="max-w-md">
-							<div className="mb-2 block">
-								<Label
-									htmlFor="file"
-									value="Profile Picture"
-									className="block text-gray-700 text-sm font-bold mb-2"
-								/>
-							</div>
-							<FileInput
-								id="file"
-								theme={inputTheme}
-								color={"purple"}
-								accept="image/*"
-								onChange={handleFileChange}
-								// helperText="A profile picture is useful to confirm your are logged into your account"
-							/>
-						</div>
+      reader.readAsDataURL(event.target.files[0]);
+    }
+  };
 
-						{/* <div className="flex items-center justify-center w-full">
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const user = localStorage.getItem("user");
+      const sessionToken = user
+        ? JSON.parse(user).authentication.sessionToken
+        : null;
+
+      try {
+        const response = await fetch(`${API_BASE_URL}/users/me`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `${sessionToken}`,
+          },
+        });
+
+        const data = await response.json();
+        console.log(data);
+        if (data.user == null) {
+          localStorage.removeItem("token");
+          localStorage.removeItem("user");
+          return;
+        }
+        setUser(data.user);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  return (
+    <div className="flex justify-center bg-gradient-to-br from-purple-400 to-indigo-600 dark:from-purple-800 dark:to-indigo-900 py-10 ">
+      <div className="w-full max-w-md ">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-white shadow rounded px-8 pt-6 pb-8 mb-4 dark:bg-gray-700"
+        >
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2 dark:text-white"
+              htmlFor="username"
+            >
+              Username
+            </label>
+            <TextInput
+              name="username"
+              placeholder="Username"
+              type="text"
+              value={user.username}
+              onChange={handleChange}
+              theme={inputTheme}
+              color={"purple"}
+              // className="border-purple-500 bg-purple-50 text-purple-900 placeholder-purple-700 focus:border-purple-500 focus:ring-purple-500 dark:border-purple-400 dark:bg-purple-100 dark:focus:border-purple-500 dark:focus:ring-purple-500"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2 dark:text-white"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <TextInput
+              name="email"
+              placeholder="Email"
+              type="email"
+              value={user.email}
+              onChange={handleChange}
+              theme={inputTheme}
+              color={"purple"}
+              className="appearance-none rounded w-full py-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline "
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2 dark:text-white"
+              htmlFor="firstName"
+            >
+              First Name
+            </label>
+            <TextInput
+              name="firstName"
+              placeholder="First Name"
+              type="text"
+              value={user.profile.firstName}
+              onChange={handleChange}
+              theme={inputTheme}
+              color={"purple"}
+              className="appearance-none rounded w-full py-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2 dark:text-white"
+              htmlFor="lastName"
+            >
+              Last Name
+            </label>
+            <TextInput
+              name="lastName"
+              placeholder="Last Name"
+              type="text"
+              value={user.profile.lastName}
+              onChange={handleChange}
+              theme={inputTheme}
+              color={"purple"}
+              className="appearance-none rounded w-full py-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2 dark:text-white"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <TextInput
+              name="password"
+              placeholder="Password"
+              type="password"
+              //   value={user.authentication.password}
+              onChange={handleChange}
+              theme={inputTheme}
+              color={"purple"}
+              className="appearance-none rounded w-full py-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="mb-4">
+            <div id="fileUpload" className="max-w-md">
+              <div className="mb-2 block">
+                <Label
+                  htmlFor="file"
+                  value="Profile Picture"
+                  className="block text-gray-700 text-sm font-bold mb-2 dark:text-white"
+                />
+              </div>
+              <FileInput
+                id="file"
+                theme={inputTheme}
+                color={"purple"}
+                accept="image/*"
+                onChange={handleFileChange}
+                // helperText="A profile picture is useful to confirm your are logged into your account"
+              />
+            </div>
+
+            {/* <div className="flex items-center justify-center w-full">
 							<label
 								htmlFor="dropzone-file"
 								className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
@@ -215,38 +316,39 @@ const SettingsPage: React.FC = () => {
 								/>
 							</label>
 						</div> */}
-					</div>
-					<div className="mb-4">
-						<label
-							className="block text-gray-700 text-sm font-bold mb-2"
-							htmlFor="bio"
-						>
-							Bio
-						</label>
-						<TextInput
-							name="bio"
-							placeholder="Bio"
-							type="text"
-							value={user.bio}
-							onChange={handleChange}
-							theme={inputTheme}
-							color={"purple"}
-							className="appearance-none rounded w-full py-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-						/>
-					</div>
-					<div className="flex items-center justify-between">
-						<Button
-							type="submit"
-							color="purple"
-							className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-						>
-							Save Changes
-						</Button>
-					</div>
-				</form>
-			</div>
-		</div>
-	);
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2 dark:text-white"
+              htmlFor="bio"
+            >
+              Bio
+            </label>
+            <TextInput
+              name="bio"
+              placeholder="Bio"
+              type="text"
+              value={user.profile.bio}
+              onChange={handleChange}
+              theme={inputTheme}
+              color={"purple"}
+              className="appearance-none rounded w-full py-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <Button
+              type="submit"
+              color="purple"
+              className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline  dark:text-white"
+            >
+              Save Changes
+            </Button>
+          </div>
+        </form>
+      </div>
+      <ToastContainer />
+    </div>
+  );
 };
 
 export default SettingsPage;
