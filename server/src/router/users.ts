bfcc9e4637aa8d34c92ae238711dba196d1c9461
Fillonit Ibishi <filloniti@gmail.com>
2024-01-13 22:27:57 +0100

    Refactor user router to improve code readability

--- server/src/router/users.ts
+++ server/src/router/users.ts
@@ -17,6 +17,11 @@ export default (router: express.Router) => {
 	router.get("/users/:id", getUser);
 	router.delete("/users/:id/admin", isAuthenticated, isAdmin, removeAdmin);
 	router.post("/users/:id/admin", isAuthenticated, isAdmin, addAdmin);
-	router.delete("/users/:id", isAuthenticated,  isOwner || isAdmin, deleteUser);
-	router.patch("/users/:id", isAuthenticated, isOwner, updateUser);
+	router.delete(
+		"/users/:id",
+		isAuthenticated,
+		isAdmin || isOwner,
+		deleteUser
+	);
+	router.patch("/users/:id", isAuthenticated, isAdmin || isOwner, updateUser);
 };
