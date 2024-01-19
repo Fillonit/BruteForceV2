fddd037137419f19d7e104b73f5edb0178d527f8
butrinnt_ <butritn.reqica@gmail.com>
2024-01-19 23:23:59 +0100

    made dashboard for games and posts, fixed some things

--- client/src/components/Admin/Dashboard.tsx
+++ client/src/components/Admin/Dashboard.tsx
@@ -1,7 +1,11 @@
 import { API_BASE_URL } from "../../config";
 import React, { useEffect, useState } from "react";
 // import Sidebar from "../../components/Admin/Sidebar";
-import UsersTable from "../../components/Admin/Table";
+import UsersTable from "./UserTable";
+import GameTable from "./GameTable";
+import PostTable from "./PostTable";
+import SidebarComponent from "./Sidebar";
+import { useParams } from "react-router-dom";
 
 interface UsersData {
   username: string;
@@ -22,6 +26,39 @@ interface UsersData {
   createdAt: string;
   updatedAt: string;
   __v: number;
+}
+
+interface GamesData {
+  ceatedAt: string;
+  description: string;
+  developer: string;
+  genre: string[];
+  image: string;
+  name: string;
+  platform: string[];
+  price: number;
+  publisher: string;
+  rating: number;
+  releaseDate: string;
+  tags: string[];
+  updatedAt: string;
+  _v: number;
+  _id: string;
+}
+
+interface PostsData {
+  author: UsersData;
+  comments: string[];
+  content: string;
+  createdAt: string;
+  imageURL: string;
+  likes: number;
+  tags: string[];
+  title: string;
+  updatedAt: string;
+  views: number;
+  _v: number;
+  _id: string;
 }
 
 // interface Author {
@@ -55,33 +92,61 @@ interface UsersData {
 
 const UsersInfo: React.FC = () => {
   const [users, setUsers] = useState<UsersData[]>([]);
+  const [games, setGames] = useState<GamesData[]>([]);
+  const [posts, setPosts] = useState<PostsData[]>([]);
 
+  const { table } = useParams();
   useEffect(() => {
-    const fetchUsers = async () => {
+    const fetchData = async () => {
       try {
-        const response = await fetch(`${API_BASE_URL}/users`, {
+        const responseUsers = await fetch(`${API_BASE_URL}/users`, {
           headers: {
             "Content-Type": "application/json",
             Authorization: `${localStorage.getItem("token")}`,
           },
         });
-        const data = await response.json();
-        setUsers(data.users);
+        const responseGames = await fetch(`${API_BASE_URL}/games`, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `${localStorage.getItem("token")}`,
+          },
+        });
+        const responsePosts = await fetch(`${API_BASE_URL}/posts`, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `${localStorage.getItem("token")}`,
+          },
+        });
+
+        const dataUsers = await responseUsers.json();
+        const dataGames = await responseGames.json();
+        const dataPosts = await responsePosts.json();
+        setPosts(dataPosts.posts);
+        setGames(dataGames.games);
+        setUsers(dataUsers.users);
       } catch (error) {
-        console.error("Failed to fetch users:", error);
+        console.error("Failed to fetch data:", error);
       }
     };
-    fetchUsers();
+    fetchData();
   }, []);
-
+  console.log(posts);
   return (
     <div>
       <div className="flex h-screen bg-gray-100 bg-gradient-to-br from-purple-400 to-indigo-600 dark:from-purple-800 dark:to-indigo-900 py-10 ">
-        {/* <div className="w-64 bg-white p-4 border-r dark:bg-gray-800">
-					<Sidebar />
-				</div> */}
-        <div className="flex-1 p-6 overflow-auto">
-          <UsersTable users={users} />
+        <div className="w-64 bg-white p-4 border-r dark:bg-gray-800">
+          <SidebarComponent />
+        </div>
+        <div className="flex-1 p-6 overflow-auto overflow-y-hidden">
+          {table === "users" || table === undefined ? (
+            <UsersTable users={users} />
+          ) : table === "games" ? (
+            <GameTable games={games} />
+          ) : table === "posts" ? (
+            <PostTable posts={posts} />
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </div>
