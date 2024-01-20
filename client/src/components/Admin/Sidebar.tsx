86ca31aaa62ed356aeab19f0b34d8356dadae8ca
Fillonit Ibishi <filloniti@gmail.com>
2024-01-20 17:31:39 +0100

    Add logo to Sidebar component

--- client/src/components/Admin/Sidebar.tsx
+++ client/src/components/Admin/Sidebar.tsx
@@ -4,6 +4,7 @@
 
 import { Sidebar } from "flowbite-react";
 import { HiChartPie, HiInbox, HiShoppingBag, HiUser } from "react-icons/hi";
+import iconDark from "../../assets/iconDark.png";
 // import { twMerge } from "tailwind-merge";
 
 function SidebarComponent() {
@@ -12,6 +13,9 @@ function SidebarComponent() {
 			aria-label="Sidebar with multi-level dropdown example"
 			className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
 		>
+			<Sidebar.Logo href="#" img={iconDark} imgAlt="Brute Force logo">
+				BruteForce
+			</Sidebar.Logo>
 			<Sidebar.Items>
 				<Sidebar.ItemGroup>
 					<Sidebar.Item href="#" icon={HiChartPie}>
