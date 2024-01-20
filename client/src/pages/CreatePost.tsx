860523750169bd5fc7e8242a8c60d8d452b697a6
Erblin <erbliny22@gmail.com>
2024-01-20 16:20:41 +0100

    Commit

---
+++ client/src/pages/CreatePost.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AddPost from "../components/AddPost/AddPost";
+
+const CreatePost = () => {
+  return (
+    <div>
+      <AddPost />
+    </div>
+  );
+};
+
+export default CreatePost;
