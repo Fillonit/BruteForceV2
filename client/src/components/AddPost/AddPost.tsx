219a4545a59fcb93b230d8d40e6243ed0f700e21
Erblin <erbliny22@gmail.com>
2024-01-21 12:52:08 +0100

    Create Post

--- client/src/components/AddPost/AddPost.tsx
+++ client/src/components/AddPost/AddPost.tsx
@@ -1,145 +1,151 @@
 import React, { useState } from "react";
 import { API_BASE_URL } from "../../config";
 import { FileInput, Label, TextInput } from "flowbite-react";
+import { useNavigate } from "react-router-dom";
 
 const inputTheme = {
-	field: {
-		input: {
-			colors: {
-				purple: "border-purple-500 bg-purple-50 text-purple-900 placeholder-purple-700 focus:border-purple-500 focus:ring-purple-500 dark:border-purple-400 dark:bg-purple-100 dark:focus:border-purple-500 dark:focus:ring-purple-500 ",
-			},
-		},
-	},
+  field: {
+    input: {
+      colors: {
+        purple:
+          "border-purple-500 bg-purple-50 text-purple-900 placeholder-purple-700 focus:border-purple-500 focus:ring-purple-500 dark:border-purple-400 dark:bg-purple-100 dark:focus:border-purple-500 dark:focus:ring-purple-500 ",
+      },
+    },
+  },
 };
 
 const AddPost = () => {
-	const [title, setTitle] = useState("");
-	const [content, setContent] = useState("");
-	const [imageURL, setImageURL] = useState("");
-	const [tags, setTags] = useState("");
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [imageURL, setImageURL] = useState("");
+  const [tags, setTags] = useState("");
+  const navigate = useNavigate();
 
-	const handleFormSubmit = async (e: React.FormEvent) => {
-		e.preventDefault();
+  const handleFormSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
 
-		try {
-			const response = await fetch(`${API_BASE_URL}/posts`, {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-					Authorization: `${localStorage.getItem("token")}`,
-				},
-				body: JSON.stringify({
-					title,
-					content,
-					imageURL,
-					tags: tags.split(","),
-				}),
-			});
+    try {
+      const response = await fetch(`${API_BASE_URL}/posts`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `${localStorage.getItem("token")}`,
+        },
+        body: JSON.stringify({
+          title,
+          content,
+          imageURL,
+          tags: tags.split(","),
+        }),
+      });
 
-			if (response.ok) {
-				const result = await response.json();
-				console.log("Post created successfully:", result.post);
-			} else {
-				const error = await response.json();
-				console.error("Error creating post:", error.message);
-			}
-		} catch (error) {
-			console.error("Internal server error:", error);
-		}
-	};
+      if (response.ok) {
+        const result = await response.json();
+        console.log("Post created successfully:", result.post);
+        navigate("/posts");
+      } else {
+        const error = await response.json();
+        console.error("Error creating post:", error.message);
+      }
+    } catch (error) {
+      console.error("Internal server error:", error);
+    }
+  };
 
-	const handleImageFileChange = (
-		event: React.ChangeEvent<HTMLInputElement>
-	) => {
-		if (event.target.files && event.target.files[0]) {
-			const reader = new FileReader();
+  const handleImageFileChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
 
-			reader.onload = (e) => {
-				setImageURL(e.target?.result as string);
-			};
+      reader.onload = (e) => {
+        setImageURL(e.target?.result as string);
+      };
 
-			reader.readAsDataURL(event.target.files[0]);
-		}
-	};
+      reader.readAsDataURL(event.target.files[0]);
+    }
+  };
 
-	return (
-		<div className="flex h-screen bg-gray-100 bg-gradient-to-br from-purple-400 to-indigo-600 dark:from-purple-800 dark:to-indigo-900 py-10 ">
-			<form onSubmit={handleFormSubmit}>
-				<div className="mb-4">
-					<label
-						className="block text-gray-700 text-sm font-bold mb-2 dark:text-white"
-						htmlFor="title"
-					>
-						Title:
-					</label>
-					<TextInput
-						name="title"
-						placeholder="Title"
-						type="text"
-						value={title}
-						onChange={(e) => setTitle(e.target.value)}
-						theme={inputTheme}
-						color={"purple"}
-					/>
-				</div>
-				<div className="mb-4">
-					<label
-						className="block text-gray-700 text-sm font-bold mb-2 dark:text-white"
-						htmlFor="content"
-					>
-						Content:
-					</label>
-					<textarea
-						value={content}
-						onChange={(e) => setContent(e.target.value)}
-						className="appearance-none rounded w-full py-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-					/>
-				</div>
-				<div className="mb-4">
-					<div id="fileUpload" className="max-w-md">
-						<div className="mb-2 block">
-							<Label
-								htmlFor="file"
-								value="Profile Picture"
-								className="block text-gray-700 text-sm font-bold mb-2 dark:text-white"
-							/>
-						</div>
-						<FileInput
-							id="file"
-							theme={inputTheme}
-							color={"purple"}
-							accept="image/*"
-							onChange={handleImageFileChange}
-							// helperText="A profile picture is useful to confirm your are logged into your account"
-						/>
-					</div>
-				</div>
-				<div className="mb-4">
-					<label
-						className="block text-gray-700 text-sm font-bold mb-2 dark:text-white"
-						htmlFor="tags"
-					>
-						Tags:
-					</label>
-					<TextInput
-						name="tags"
-						placeholder="Tags"
-						type="text"
-						value={tags}
-						onChange={(e) => setTags(e.target.value)}
-						theme={inputTheme}
-						color={"purple"}
-					/>
-				</div>
-				<button
-					type="submit"
-					className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline dark:text-white"
-				>
-					Create Post
-				</button>
-			</form>
-		</div>
-	);
+  return (
+    <div className="flex h-screen bg-[url('https://i.pinimg.com/originals/d9/1a/d7/d91ad79a3f6b1075fc48d63fd5f71afb.gif')] bg-no-repeat bg-cover justify-center items-center">
+      <form
+        onSubmit={handleFormSubmit}
+        className="bg-white dark:bg-gray-800 p-8 rounded shadow-md w-1/2"
+      >
+        <h4 className="text-2xl dark:text-white text-purple-700 mb-6">
+          CREATE A POST
+        </h4>
+
+        <div className="mb-4">
+          <label
+            className="block dark:text-white text-gray-700 text-sm font-bold mb-2"
+            htmlFor="title"
+          >
+            Title:
+          </label>
+          <TextInput
+            name="title"
+            placeholder="Title"
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            theme={inputTheme}
+            color={"purple"}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block dark:text-white text-gray-700 text-sm font-bold mb-2"
+            htmlFor="content"
+          >
+            Content:
+          </label>
+          <textarea
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            className="w-full px-3 py-4 border rounded focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="mb-4">
+          <Label
+            htmlFor="file"
+            value="Upload Image"
+            className="block dark:text-white text-gray-700 text-sm font-bold mb-2"
+          />
+          <FileInput
+            id="file"
+            theme={inputTheme}
+            color={"purple"}
+            accept="image/*"
+            onChange={handleImageFileChange}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block dark:text-white text-gray-700 text-sm font-bold mb-2"
+            htmlFor="tags"
+          >
+            Tags:
+          </label>
+          <TextInput
+            name="tags"
+            placeholder="Tags"
+            type="text"
+            value={tags}
+            onChange={(e) => setTags(e.target.value)}
+            theme={inputTheme}
+            color={"purple"}
+          />
+        </div>
+        <button
+          type="submit"
+          className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Create Post
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default AddPost;
--- client/src/components/User/NavAvatar.tsx
+++ client/src/components/User/NavAvatar.tsx
@@ -7,98 +7,95 @@ import notifyConfig from "../notifyConfig";
 import { Link } from "react-router-dom";
 
 interface UserData {
-	username: string;
-	email: string;
-	profile: {
-		avatar: string;
-	};
+  username: string;
+  email: string;
+  profile: {
+    avatar: string;
+  };
 }
 
 function NavAvatar() {
-	const [userData, setUserData] = useState<UserData | null>(null);
+  const [userData, setUserData] = useState<UserData | null>(null);
 
-	function handleSignOut() {
-		localStorage.removeItem("token");
-		localStorage.removeItem("user");
-		toast.success("Signed out successfully!", notifyConfig as ToastOptions);
-		setTimeout(() => {
-			window.location.href = "/login";
-		}, 1000);
-	}
+  function handleSignOut() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    toast.success("Signed out successfully!", notifyConfig as ToastOptions);
+    setTimeout(() => {
+      window.location.href = "/login";
+    }, 1000);
+  }
 
-	useEffect(() => {
-		const fetchUserData = async () => {
-			const user = localStorage.getItem("user");
-			const sessionToken = user
-				? JSON.parse(user).authentication.sessionToken
-				: null;
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const user = localStorage.getItem("user");
+      const sessionToken = user
+        ? JSON.parse(user).authentication.sessionToken
+        : null;
 
-			try {
-				const response = await fetch(`${API_BASE_URL}/users/me`, {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-						Authorization: `${sessionToken}`,
-					},
-				});
+      try {
+        const response = await fetch(`${API_BASE_URL}/users/me`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `${sessionToken}`,
+          },
+        });
 
-				const data = await response.json();
-				console.log(data);
-				if (data.user == null) {
-					localStorage.removeItem("token");
-					localStorage.removeItem("user");
-					return;
-				}
-				setUserData(data.user);
-			} catch (error) {
-				console.error(error);
-			}
-		};
+        const data = await response.json();
+        console.log(data);
+        if (data.user == null) {
+          localStorage.removeItem("token");
+          localStorage.removeItem("user");
+          return;
+        }
+        setUserData(data.user);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-		fetchUserData();
-	}, []);
+    fetchUserData();
+  }, []);
 
-	if (!userData) {
-		return null;
-	}
+  if (!userData) {
+    return null;
+  }
 
-	return (
-		<div>
-			<Dropdown
-				label={
-					<Avatar
-						alt="User settings"
-						img={userData.profile.avatar}
-						rounded
-					/>
-				}
-				arrowIcon={false}
-				inline
-			>
-				<Dropdown.Header>
-					<span className="block text-sm font-bold">
-						{userData.username}
-					</span>
-					<span className="block truncate text-sm font-medium">
-						{userData.email}
-					</span>
-				</Dropdown.Header>
-				<Link to={`/profile`}>
-					<Dropdown.Item>Profile</Dropdown.Item>
-				</Link>
-				<Link to={`/dashboard/users`}>
-					<Dropdown.Item>Dashboard</Dropdown.Item>
-				</Link>
-				<Link to={`/settings`}>
-					<Dropdown.Item>Settings</Dropdown.Item>
-				</Link>
-				{/* <Dropdown.Item>Posts</Dropdown.Item> */}
-				<Dropdown.Divider />
-				<Dropdown.Item onClick={handleSignOut}>Sign out</Dropdown.Item>
-			</Dropdown>
-			<ToastContainer />
-		</div>
-	);
+  return (
+    <div>
+      <Dropdown
+        label={
+          <Avatar alt="User settings" img={userData.profile.avatar} rounded />
+        }
+        arrowIcon={false}
+        inline
+      >
+        <Dropdown.Header>
+          <span className="block text-sm font-bold">{userData.username}</span>
+          <span className="block truncate text-sm font-medium">
+            {userData.email}
+          </span>
+        </Dropdown.Header>
+        <Link to={`/profile`}>
+          <Dropdown.Item>Profile</Dropdown.Item>
+        </Link>
+        <Link to={`/createpost`}>
+          <Dropdown.Item>Create Post</Dropdown.Item>
+        </Link>
+        <Link to={`/dashboard/users`}>
+          <Dropdown.Item>Dashboard</Dropdown.Item>
+        </Link>
+        <Link to={`/settings`}>
+          <Dropdown.Item>Settings</Dropdown.Item>
+        </Link>
+        {/* <Dropdown.Item>Posts</Dropdown.Item> */}
+        <Dropdown.Divider />
+        <Dropdown.Item onClick={handleSignOut}>Sign out</Dropdown.Item>
+      </Dropdown>
+      <ToastContainer />
+    </div>
+  );
 }
 
 export default NavAvatar;
